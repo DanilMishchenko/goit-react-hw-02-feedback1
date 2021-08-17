a56320aa7e57a8dd369897cba7c88d6661196c21
Danil Mishchenko <dan5679mi@gmail.com>
2021-08-18 02:24:00 +0300

    Первый этап

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+import "modern-normalize/modern-normalize.css";
+import "./styles.css";
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleClick = (event) => {
+    const review = event.target.textContent;
+    this.setState({ [review]: this.state[review] + 1 });
+  };
+
+  render() {
+    return (
+      <div className="feedback">
+        <h2>Please leave feedback</h2>
+        <div>
+          <button type="button" onClick={this.handleClick}>
+            good
+          </button>
+          <button type="button" onClick={this.handleClick}>
+            neutral
+          </button>
+          <button type="button" onClick={this.handleClick}>
+            bad
+          </button>
+        </div>
+        <h2>Statistics</h2>
+        <div>
+          <ul>
+            <li>Good:{this.state.good}</li>
+            <li>Neutral:{this.state.neutral}</li>
+            <li>Bad:{this.state.bad}</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
